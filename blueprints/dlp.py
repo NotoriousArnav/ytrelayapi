b4a6ae827943955bb8b6e4f5f97434a5816afde2
Arnav Ghosh <arnavghosh.official.2004@gmail.com>
2024-02-15 19:09:07 +0530

    Added Docs

--- blueprints/dlp.py
+++ blueprints/dlp.py
@@ -9,10 +9,32 @@ api = Blueprint(
 )
 
 def generate_data_from_response(resp, chunk=2048):
+    """
+Generate data chunks from a response object.
+
+Args:
+- resp (requests.Response): The response object to generate data from.
+- chunk (int): The chunk size in bytes (default is 2048).
+
+Yields:
+- bytes: Data chunk from the response.
+    """
     for data_chunk in resp.iter_content(chunk_size=chunk):
         yield data_chunk
 
 def serve_partial(url, range_header, mime, size=3145728):
+    """
+Serve partial content from a URL.
+
+Args:
+- url (str): The URL of the content.
+- range_header (str): The Range header string specifying the byte range to fetch.
+- mime (str): The MIME type of the content.
+- size (int): The default size in bytes if until_bytes is not specified (default is 3145728).
+
+Returns:
+- flask.Response: The response object with the streamed content.
+    """
     from_bytes, until_bytes = range_header.replace('bytes=', '').split('-')
     if not until_bytes:
         until_bytes = int(from_bytes) + size  # Default size is 3MB
@@ -28,6 +50,12 @@ def serve_partial(url, range_header, mime, size=3145728):
 
 @api.route('/audio')
 def fetch_audio():
+    """
+Fetch audio data from a YouTube video stream URL and serve it to the client.
+
+Returns:
+- flask.Response: The response object with the streamed audio data.
+    """
     video_id = request.args.get('videoId')
     try:
         video = YouTube(f'https://music.youtube.com/watch?v={video_id}')
